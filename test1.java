f29763eed451699ad06fcef05f2dccd5c3d6fc83
Bethi777 <150098666+Bethi777@users.noreply.github.com>
2023-11-06 21:42:35 -0500

    My first unit test

---
+++ test1.java
@@ -0,0 +1,7 @@
+public class test1 extends addition{
+    public static void main(String[] args) {
+        int add=sum(1,2);
+        System.out.println(add);
+        System.out.println("the sum is 3");
+    }
+}
