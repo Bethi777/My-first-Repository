4dceffbda3187dacaf2f143641501e748d5b6a50
Bethi777 <150098666+Bethi777@users.noreply.github.com>
2023-11-06 21:43:16 -0500

    My second unit test

---
+++ Test2.java
@@ -0,0 +1,7 @@
+public class Test2 extends addition {
+    public static void main(String[] args) {
+        int add=sum(5,9);
+        System.out.println(add);
+        System.out.println("The sum should be 14");
+    }
+}
